49810bef9e26e9befe0f1dd6f83fff0a1c899151
sanchez005909 <sanchez005909@gmail.com>
2023-08-29 16:00:01 +0300

    add parsing superJob

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,28 @@
-def user_interaction():
-    platforms = ["HeadHunter", "SuperJob"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: ")
+def get_filtering_data():
+    """
+    Работа с пользователем
+    :return: 1) Поисковый запрос
+    2) Нужна ли сортировка
+    3) количество топ-вакансий
+    """
+    search_word = input('Введите поисковый запрос:')
+
+    sorted_list = 0
+    while int(sorted_list) not in (1, 2):
+        sorted_list = input('Отсортировать список по ЗП? Введите:(1 - да, 2 - нет)')
+        if sorted_list.isdigit():
+            if int(sorted_list) not in (1, 2):
+                print('Введено некорректное значение')
+                print('Повторите попытку')
+        else:
+            sorted_list = 0
+            print('Введено некорректное значение')
+            print('Повторите попытку')
+
+    while True:
+        count_top_vacancies = input('Введите количество топ-вакансий для вывода (целое число)')
+        if count_top_vacancies.isdigit():
+            return search_word, sorted_list, count_top_vacancies
+        else:
+            print('Введено некорректное значение')
+            print('Повторите попытку')
